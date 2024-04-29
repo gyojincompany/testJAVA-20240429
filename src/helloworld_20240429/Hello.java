2fc5769b69756205a5f512aabf2f74e40b93e5d1
gyojincompany <luckyred@gmail.com>
2024-04-29 16:19:54 +0900

    주석 추가 연습

--- src/helloworld_20240429/Hello.java
+++ src/helloworld_20240429/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld!!"); // 콘솔에 결과를 프린트->주의:자바는 ""만 문자열에 사용
 		System.out.println("안녕하세요!!");
+		System.out.println("안녕하세요!!");
 	}
 
 }
