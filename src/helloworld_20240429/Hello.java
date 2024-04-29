9205023209eb2b4ac6b6c8168d6a636d8a6f55c3
gyojincompany <luckyred@gmail.com>
2024-04-29 15:34:14 +0900

    자바 기본 문법 예제 추가

---
+++ src/helloworld_20240429/Hello.java
@@ -0,0 +1,11 @@
+package helloworld_20240429;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld!!"); // 콘솔에 결과를 프린트->주의:자바는 ""만 문자열에 사용
+		System.out.println("안녕하세요!!");
+	}
+
+}
